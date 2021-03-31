4b80123017ca435b33cf4aafa4922e6ef6521699
elistjames <78109485+elistjames@users.noreply.github.com>
2021-03-30 23:32:21 -0600

    commit message by me

--- src/edu/ucalgary/ensf409/CreateOrder.java
+++ src/edu/ucalgary/ensf409/CreateOrder.java
@@ -7,6 +7,8 @@ public class CreateOrder {
     private PrintWriter outStream;
     private String[] itemsOrdered;
     private String totalPrice;
+    private int orderID;
+    Database db;
 
     CreateOrder(Order request, int orderNumber, Database db)
     {
@@ -21,6 +23,7 @@ public class CreateOrder {
             System.out.println("error opening output file");
             System.exit(1);
         }
+        this.db = db;
     }
     public void setOriginalRequest(Order request){
         this.originalRequest = request;
@@ -61,5 +64,75 @@ public class CreateOrder {
         }
         outStream.println();
         outStream.println("Total Price: " + this.totalPrice);
+    }
+
+
+    public int getLowestPrice(){
+        int lowest = 0;
+        if(prices.size() >= 1){
+            lowest = prices.get(0);
+        }
+        for(int i = 0; i < prices.size(); i++){
+            if(prices.get(i) <= lowest){
+                lowest = prices.get(i);
+            }
+        }
+        prices.clear();
+        return lowest;
+    }
+
+    ArrayList<Integer>prices = new ArrayList<Integer>();
+
+
+    public void chairPrice(int priceTotal, ArrayList<Integer>alreadyHit, String type, int number, int legs, int arms, int seats, int cushions) {
+        int totalPrice2 = priceTotal;
+        boolean newEvent;
+        for (int i = 0; i < db.getChairs().length; i++) {
+            int lCount = legs;
+            int aCount = arms;
+            int sCount = seats;
+            int cCount = cushions;
+            ArrayList<Integer>alreadyHit2 = new ArrayList<Integer>(alreadyHit);
+            if(db.getChairs()[i].getType().equals(type)){
+                newEvent = true;
+                for(int j = 0; j < alreadyHit.size(); j++){
+                    if(i == alreadyHit.get(j)){
+                        newEvent = false;
+                    }
+                }
+                if(newEvent){
+                    boolean used = false;
+                    alreadyHit2.add(i);
+                    if(db.getChairs()[i].getLegs().equals("Y") && legs < number){
+                        lCount = legs+1;
+                        used = true;
+                    }
+                    if(db.getChairs()[i].getArms().equals("Y") && arms < number){
+                        aCount = arms+1;
+                        used = true;
+                    }
+                    if(db.getChairs()[i].getSeat().equals("Y") && seats < number){
+                        sCount = seats+1;
+                        used = true;
+                    }
+                    if(db.getChairs()[i].getCushion().equals("Y") && cushions < number){
+                        cCount = cushions+1;
+                        used = true;
+                    }
+                    totalPrice2 = priceTotal + db.getChairs()[i].getPrice();
+                    if(lCount+aCount+sCount+cCount == number*4){
+                        prices.add(totalPrice2);
+                        return;
+                    }
+
+                    //if(used){
+                    chairPrice(totalPrice2, alreadyHit2, type, number, lCount, aCount, sCount, cCount);
+                    //}
+                    //else{
+                        //chairPrice(priceTotal, alreadyHit2, type, number, lCount, aCount, sCount, cCount);
+                    //}
+                }
+            }
+        }
     }
 }
