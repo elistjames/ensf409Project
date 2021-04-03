5c577d27b9dab1187088d83fd110fdc2fa588d6b
Syntax-Cipher <chad.holst@hotmail.com>
2021-04-03 03:15:27 -0600

    commit V1

---
+++ src/edu/ucalgary/ensf409/CreateOrderTest.java
@@ -0,0 +1,8 @@
+package edu.ucalgary.ensf409;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CreateOrderTest
+{
+
+}
---
+++ src/edu/ucalgary/ensf409/OrderTest.java
@@ -0,0 +1,8 @@
+package edu.ucalgary.ensf409;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class OrderTest
+{
+    // input takes in wrong spelling of type input for and sends order out with the wrong spelling. (CreateOrder makes document)
+}
