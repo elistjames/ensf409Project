3f9e9b165af541bf9802ca72f3122f521dacfc84
elistjames <78109485+elistjames@users.noreply.github.com>
2021-04-05 15:30:17 -0600

    commit by group

---
+++ src/edu/ucalgary/ensf409/OrderTest.java
@@ -0,0 +1,11 @@
+package edu.ucalgary.ensf409;
+import org.junit.*;
+import java.sql.*;
+
+public class OrderTest {
+    // userCategory
+    // userType
+    // userNumber
+    // make another order
+
+}
